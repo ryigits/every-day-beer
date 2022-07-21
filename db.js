bfc8cf63e76e4add81e60cc5402b01b9d82629ce
Yigit <ryigit@gmail.com>
2022-07-21 15:37:41 +0200

    Revert "new db created. db.js revised"
    This reverts commit 53135ab880855d09fb2d1635b09468b6be9ee92a.

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 const spicedPg = require("spiced-pg");
 const db = spicedPg("postgres:postgres:postgres@localhost:5432/ryigit");
 
-module.exports.addMember = (first_name, last_name, email, password) => {
+module.exports.addMember = (name, lname, password, date, email) => {
     return db.query(
         `
-        INSERT INTO users(first_name,last_name,email,password_hash)
-        VALUES ($1,$2,$3,$4) RETURNING id`,
-        [first_name, last_name, email, password]
+        INSERT INTO members(name,lname,password,date,email)
+        VALUES ($1,$2,$3,$4,$5) RETURNING id`,
+        [name, lname, password, date, email]
     );
 };
 
 module.exports.getAllMembers = () => {
-    return db.query(`SELECT * FROM users`);
+    return db.query(`SELECT * FROM members`);
 };
 
 module.exports.getMembersByEmail = (email) => {
     return db.query(
         `
-        SELECT * FROM users WHERE email=$1`,
+        SELECT * FROM members WHERE email=$1`,
         [email]
     );
 };
@@ -25,17 +25,17 @@ module.exports.getMembersByEmail = (email) => {
 module.exports.getMembersById = (id) => {
     return db.query(
         `
-        SELECT * FROM users WHERE id=$1`,
+        SELECT * FROM members WHERE id=$1`,
         [id]
     );
 };
 
-module.exports.addSignature = (user_id, url) => {
+module.exports.addSignature = (id, url) => {
     return db.query(
         `
-        INSERT INTO signatures(user_id,signature)
-        VALUES ($1,$2) RETURNING signature`,
-        [user_id, url]
+        INSERT INTO signatures(id,url)
+        VALUES ($1,$2) RETURNING url`,
+        [id, url]
     );
 };
 
@@ -43,13 +43,5 @@ module.exports.getAllSignatures = () => {
     return db.query(
         `
         SELECT * FROM signatures`
-    );
-};
-
-module.exports.getSignature = (id) => {
-    return db.query(
-        `
-        SELECT * FROM signatures WHERE user_id=$1`,
-        [id]
     );
 };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+
+const moment = require("moment");
 const hb = require("express-handlebars");
 const db = require("./db");
 const bcrypt = require("./bcrypt");
@@ -27,7 +29,15 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-    if (req.session.signatureId) {
+    if (req.session.signatureId && req.session.signed) {
+        db.getMembersById(req.session.signatureId).then(() => {
+            res.render("thanks", {
+                logged: true,
+                name: req.session.name,
+                totalNumber: req.session.list.length,
+            });
+        });
+    } else if (req.session.signatureId && !req.session.signed) {
         res.redirect(302, "/petition");
     } else {
         res.render("home", {
@@ -53,7 +63,7 @@ app.post("/login", (req, res) => {
             return res.render("login", {
                 doesNotMatch: true,
             });
-        const hash = result.rows[0].password_hash;
+        const hash = result.rows[0].password;
         bcrypt.compare(req.body.password, hash).then((result) => {
             if (result) {
                 return res.redirect("/petition");
@@ -66,29 +76,28 @@ app.post("/login", (req, res) => {
         let member = result.rows[0];
         req.session.signed = false;
         req.session.signatureId = member.id;
-        req.session.name = member.first_name;
+        req.session.name = member.name;
     });
 });
 
 app.get("/petition", (req, res) => {
-    db.getSignature(req.session.signatureId)
-        .then((result) => {
-            req.session.signature = result.rows[0].signature;
-            return res.render("thanks", {
-                logged: true,
-                name: req.session.name,
-                signature: req.session.signature,
-                totalNumber: req.session.list.length,
-            });
-        })
-        .catch(() => {
-            return res.render("petition", {
-                logged: true,
-                name: req.session.name,
-                totalNumber: req.session.list.length,
-            });
+    if (req.session.signatureId && req.session.signed) {
+        res.render("thanks", {
+            logged: true,
+            name: req.session.name,
+            totalNumber: req.session.list.length,
         });
+    } else if (req.session.signatureId) {
+        res.render("petition", {
+            logged: true,
+            name: req.session.name,
+            totalNumber: req.session.list.length,
+        });
+    } else {
+        res.redirect(302, "/");
+    }
 });
+
 app.get("/login", (req, res) => {
     if (req.session.signatureId)
         return res.render("login", {
@@ -111,8 +120,10 @@ app.post("/", (req, res) => {
         return res.render("home", {
             showPasswordError: true,
         });
+    let date = moment().format();
+    let email = req.body.email;
     bcrypt.hash(req.body.password).then((password) => {
-        db.addMember(req.body.fname, req.body.lname, req.body.email, password)
+        db.addMember(req.body.fname, req.body.lname, password, date, email)
             .then((result) => {
                 return result.rows;
             })
@@ -131,10 +142,10 @@ app.post("/", (req, res) => {
 
 app.post("/petition", (req, res) => {
     // add signature page
-    if (req.session.signatureId) {
+    if (req.session.signatureId && !req.session.signed) {
         setTimeout(() => {
-            db.addSignature(req.session.signatureId, req.body.url)
-                .then((url) => {
+            db.addSignature(req.session.signatureId, req.body.url).then(
+                (url) => {
                     req.session.signed = true;
                     res.render("thanks", {
                         url: url.rows[0].url,
@@ -142,27 +153,20 @@ app.post("/petition", (req, res) => {
                         totalNumber: req.session.list.length,
                         logged: true,
                     });
-                })
-                .catch(() => {
-                    res.render("petition", {
-                        showDbError: true,
-                        logged: true,
-                        name: req.session.name,
-                        totalNumber: req.session.list.length,
-                    });
-                });
+                }
+            );
         }, 500);
     }
 });
 
 app.get("/list", (req, res) => {
+    if (!req.session.signatureId) return res.redirect(302, "/");
     if (req.session.signatureId) {
         res.render("list", {
             list: req.session.list,
             logged: true,
             name: req.session.name,
             totalNumber: req.session.list.length,
-            signature: req.session.signature,
         });
     }
 });
